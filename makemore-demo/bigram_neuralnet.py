94441a0c2c4090fd1a47d8190bb7333f186d23f3
Madhav Kandukuri <madhav165@gmail.com>
2022-11-30 00:43:57 +0530

    sampling from nn

--- makemore-demo/bigram_neuralnet.py
+++ makemore-demo/bigram_neuralnet.py
@@ -30,16 +30,13 @@ yenc = F.one_hot(ys, num_classes=num_char+1).float()
 g = torch.Generator().manual_seed(2147483647)
 W = torch.randn((num_char+1, num_char+1), generator=g, requires_grad=True) # first num represents the dimension of data and second number of neurons
 
-for k in range(200):
+for k in range(300):
     #forward pass
     logits = xenc @ W # @ is a matrix multiplication operator in Pytorch. Intepreted as log counts for bigrams
     # softmax steps
     counts = logits.exp() # equivalent to N
     probs = counts / counts.sum(1, keepdims=True)
     loss = -probs[torch.arange(xs.shape[0]), ys].log().mean() + 0.01 * (W**2).mean()
-
-    print(loss.item())
-
 
     #backward pass
     W.grad = None
@@ -55,4 +52,22 @@ for k in range(200):
     probs = counts / counts.sum(1, keepdims=True)
     loss = -probs[torch.arange(xs.shape[0]), ys].log().mean()
 
+print(f'{loss.item()=}')
+
     
+# sample from the neural net model
+for i in range(20):
+    out = []
+    ix = 0
+    while True:
+        xenc = F.one_hot(torch.tensor([ix]), num_classes=num_char+1).float()
+        logits = xenc @ W
+        counts = logits.exp()
+        p = counts / counts.sum(1, keepdims=True)
+
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[ix])
+
+        if ix == 0:
+            break
+    print(''.join(out))
