6b4021dae0123bc993840a6e55211ae3d646e920
Madhav Kandukuri <madhav165@gmail.com>
2022-11-29 23:24:12 +0530

    added nll

--- makemore-demo/bigram.py
+++ makemore-demo/bigram.py
@@ -44,3 +44,19 @@ for i in range(20):
         if ix == 0:
             break
     print(''.join(out))
+
+log_likelihood = 0
+n = 0
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        prob = P[ix1, ix2]
+        logprob = torch.log(prob)
+        log_likelihood += logprob
+        n += 1
+
+nll = -log_likelihood/n # lower nll indictes a better model
+print(f'{log_likelihood=}')
+print(f'{nll=}')
