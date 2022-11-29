655921dbe45ec4b58ed8940717bb0245211e7625
Madhav Kandukuri <madhav165@gmail.com>
2022-11-29 22:57:10 +0530

    bigram model

---
+++ makemore-demo/bigram.py
@@ -0,0 +1,46 @@
+import torch
+import matplotlib.pyplot as plt
+
+words = open('sanskrit_names.txt', 'r').read().splitlines()
+
+chars = sorted(list(set(''.join(words))))
+num_char = len(chars)
+stoi = {s:i+1 for i, s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s, i in stoi.items()}
+
+N = torch.zeros((num_char+1, num_char+1), dtype=torch.int32)
+
+# bigram model
+for w in words:
+    chs = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+        N[ix1, ix2] += 1
+
+plt.figure(figsize=(16, 16))
+plt.imshow(N, cmap='Blues')
+for i in range(num_char + 1):
+    for j in range(num_char + 1):
+        chstr = itos[i] + itos[j]
+        plt.text(j, i, chstr, ha='center', va='bottom', color='gray')
+        plt.text(j, i, N[i, j].item(), ha='center', va='top', color='gray')
+plt.axis('off')
+# plt.savefig('bigram_N.png')
+
+g = torch.Generator().manual_seed(2147483647)
+
+P = N.float()
+P /= P.sum(dim=1, keepdim=True)
+
+for i in range(20):
+    out = []
+    ix = 0
+    while True:
+        p = P[ix]
+        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+        out.append(itos[ix])
+        if ix == 0:
+            break
+    print(''.join(out))
